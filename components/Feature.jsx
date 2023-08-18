412d7be1e068f44d5289126316f810db9f82d2e2
SwiftlyBox <paymentsswiftlybox@gmail.com>
2023-08-18 15:35:36 +0200

    Gifted

---
+++ components/Feature.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+
+const Feature =()=>{
+    return (
+        <>
+            Hey
+
+        </>
+    )
+}
+
+export default Feature;
---
+++ components/index.js
@@ -0,0 +1,8 @@
+export {default  as NavBar} from "./NavBar.jsx";
+export {default as Hero} from "./Hero.jsx";
+export {default as Step} from "./Step.jsx";
+export {default as Feature} from "./Feature.jsx";
+export {default as Testimonial} from "./Testimonial.jsx";
+export {default as Stats} from "./Stats.jsx";
+export {default as Contact} from "./Contact.jsx";
+export {default as Footer} from "./Footer.jsx"
